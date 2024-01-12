1b66c6c87e3cfc1b28e607eff4b437c9da97bb9f
Harshith Muthangi <harshithm2508@gmail.com>
2024-01-12 23:00:32 +0530

    added functionality to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,14 @@ import { Todos } from './components/ToDos'
 
 function App() {
 
+  const [todos,setTodos] = useState([])
+
+  fetch("http://localhost:3000/todos")
+  .then(async (res)=>{
+      const json = await res.json();
+      setTodos(json);
+  })
+
   return (
     <>
       <CreateToDo/>
--- frontend/src/components/CreateToDo.jsx
+++ frontend/src/components/CreateToDo.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
+
 export function CreateToDo(){
+
+    const [title,setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+
     return(
         <>
-            <input style={{padding:10 , margin:10, borderRadius:"10px"}} type="text" placeholder="Enter title"/>  <br /><br />
-            <input style={{padding:10 , margin:10, borderRadius:"10px"}} type="text" placeholder="Enter description" />   <br />  <br />
+            <input style={{padding:10 , margin:10, borderRadius:"10px"}} type="text" onChange={(e)=>{
+                const value = e.target.value;
+                setTitle(value);
+            }} placeholder="Enter title"/>  <br /><br />
 
-            <button>Add To Do</button>
+            <input style={{padding:10 , margin:10, borderRadius:"10px"}} type="text" onChange={(e)=>{
+                const value = e.target.value;
+                setDescription(value);
+            }} placeholder="Enter description" />   <br />  <br />
+
+            <button onClick={()=>{
+                fetch("http://localhost:3000/todos",{
+                    method: "POST",
+                    body:JSON.stringify({
+                        title : title,
+                        description : description
+                    }),
+                    headers:{
+                        "Content-type" : "application/json"
+                    }
+                })
+                .then(async (res)=>{
+                    const json = await res.json();
+                    alert("To do added.")
+                })
+            }}>Add To Do</button>
         </>
     )
 }
